3dfbc4d73d449d3fb02becbb42dcbc587174ba61
Tianyi Liu <tliu@macalester.edu>
2017-04-20 23:56:48 -0500

    Frontend implementation

--- my_website/app.js
+++ my_website/app.js
@@ -1,3 +1,6 @@
+/**
+* Source: https://www.w3schools.com/howto/howto_js_slideshow.asp
+**/
 
 $(document).ready(function() {
   var slideIndex = 1;
@@ -20,22 +23,34 @@ $(document).ready(function() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
+    
+    timer = setTimeout(showSlides, 5000); // Change image every 4 seconds
+
     if (n > slides.length) {
       slideIndex = 1
-    } 
+      clearTimeout(timer); //resets timer 
+    }
+
     if (n < 1) {
       slideIndex = slides.length
+      clearTimeout(timer); //resets timer 
     }
+
     for (i = 0; i < slides.length; i++) {
        slides[i].style.display = "none";  
     }
-    slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1}    
+
+    if (slideIndex > slides.length) {
+      slideIndex = 1
+    }    
+
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
-}
+    timer;
+    slideIndex++;
+  }
 });
